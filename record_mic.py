56eb99a33aa2261754b32efcfc5c5310c93611f2
dannovikov <36117344+dannovikov@users.noreply.github.com>
2021-05-12 23:09:59 -0400

    Selective Track playback (WIP), infrastructure,  rerecording tracks, globalizing some vars

--- record_mic.py
+++ record_mic.py
@@ -2,52 +2,107 @@ import pyaudio
 import wave
 from threading import Thread 
 import time
+import sys
+
+TRACK_COUNT_FILE = 'track_count.txt'
+RECORD_SECONDS = 5
 
 class AudioFile:
 	# https://stackoverflow.com/a/6951154
-    chunk = 1024
+	chunk = 1024
 
-    def __init__(self, file):
-        """ Init audio stream """ 
-        self.wf = wave.open(file, 'rb')
-        self.p = pyaudio.PyAudio()
-        self.stream = self.p.open(
-            format = self.p.get_format_from_width(self.wf.getsampwidth()),
-            channels = self.wf.getnchannels(),
-            rate = self.wf.getframerate(),
-            output = True
-        )
+	def __init__(self, file):
+		""" Init audio stream """ 
+		self.wf = wave.open(file, 'rb')
+		self.p = pyaudio.PyAudio()
+		self.stream = self.p.open(
+			format = self.p.get_format_from_width(self.wf.getsampwidth()),
+			channels = self.wf.getnchannels(),
+			rate = self.wf.getframerate(),
+			output = True
+		)
 
-    def play(self):
-        """ Play entire file """
-        data = self.wf.readframes(self.chunk)
-        while data != '':
-            self.stream.write(data)
-            data = self.wf.readframes(self.chunk)
+	def play(self):
+		""" Play entire file """
+		data = self.wf.readframes(self.chunk)
+		while data != '':
+			self.stream.write(data)
+			data = self.wf.readframes(self.chunk)
+			try:
+				end = data[0]
+			except IndexError as e:
+				return
 
-    def close(self):
-        """ Graceful shutdown """ 
-        self.stream.close()
-        self.p.terminate()
+	def close(self):
+		""" Graceful shutdown """ 
+		self.stream.close()
+		self.p.terminate()
 
 def main():
-	#Record Tracks from Microphone
-	print("Recording track 1")
-	t1_file = record_mic_to_file('track1.wav', True)
-	print("Recording track 2")
-	t2_file = record_mic_to_file('track2.wav', False)
+	if 'play' in sys.argv:
+		play_tracks(sys.argv[2:])	
+	elif 'special' in sys.argv:
+		play_tracks_special()
+	else:
+		record_and_play(3)
 
-	#play two tracks back at the same time
-	t1 = AudioFile(t1_file)
-	t2 = AudioFile(t2_file)
 
-	t1_thread, t2_thread = play(t1), play(t2)
+def record_and_play(n):
+	'''
+	Waits three seconds before and between each track recording
+	plays previous tracks while recording next tracks
+	'''
+	track_files = []
+	for i in range(n):
+		print(f'Recording track #{i+1}:')
+		for j in range(3):
+			print(3-j, end = '\r')
+			time.sleep(1)
+		if track_files:
+			for track in track_files:
+				play(AudioFile(track))
+		#TODO: ask whether to redo recording here
+		track_files.append(record_mic_to_file(f'track__{i}.wav', False))
 
-	for i in range(3):
+	print('Done recording. Engaging harmonic replay...')
+	for j in range(3):
+		print(f'In {3-j}...', end = '\r')
 		time.sleep(1)
-	#ERROR: These threads dont die!!!
-	print('done')
 	
+	print('Playing...')
+	threads = []
+	for track in track_files:
+		threads.append(play(AudioFile(track)))
+
+	wait(RECORD_SECONDS)
+	print('Done.')
+
+
+def add_tracks(num_tracks):
+	track_count = get_track_count()
+	new_track_id = f'track_{track_count+1}.wav'
+
+
+def get_track_count():
+	global TRACK_COUNT_FILE
+	with open(TRACK_COUNT_FILE, 'r') as f:
+		return f.readline().strip()
+
+
+def update_track_count(n):
+	global TRACK_COUNT_FILE
+	with open(TRACK_COUNT_FILE, 'w') as f:
+		f.write(str(n))
+
+
+def play_tracks(tracks):
+	print(f'Playing track {tracks}' )
+	track_files = ['track__0.wav', 'track__1.wav', 'track__2.wav']
+	for i in tracks:
+		play(AudioFile(track_files[int(i)-1]))
+	wait(RECORD_SECONDS)
+	print('Done.')
+
 
 
 def play(A):
@@ -65,7 +120,7 @@ def record_mic_to_file(filename, choose_mic = False):
 	FORMAT = pyaudio.paInt16
 	CHANNELS = 2
 	RATE = 44100
-	RECORD_SECONDS = 5
+	global RECORD_SECONDS 
 	WAVE_OUTPUT_FILENAME = filename
 
 	p = pyaudio.PyAudio()
@@ -75,8 +130,8 @@ def record_mic_to_file(filename, choose_mic = False):
 		info = p.get_host_api_info_by_index(0)
 		numdevices = info.get('deviceCount')
 		for i in range(0, numdevices):
-		        if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
-		            print("Input Device id ", i, " - ", p.get_device_info_by_host_api_device_index(0, i).get('name'))
+				if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
+					print("Input Device id ", i, " - ", p.get_device_info_by_host_api_device_index(0, i).get('name'))
 
 		print("-------------------------------------------------------------")
 
@@ -86,20 +141,21 @@ def record_mic_to_file(filename, choose_mic = False):
 		mic_index = 1
 
 	stream = p.open(format=FORMAT,
-	                channels=CHANNELS,
-	                rate=RATE,
-	                input=True,
-	                input_device_index = mic_index,
-	                frames_per_buffer=CHUNK)
+					channels=CHANNELS,
+					rate=RATE,
+					input=True,
+					input_device_index = mic_index,
+					frames_per_buffer=CHUNK)
 
 	print("* recording")
 
 	frames = []
 
 	for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-	    data = stream.read(CHUNK)
-	    frames.append(data)
-
+		print(int( (i % int(RATE / CHUNK * RECORD_SECONDS)/RATE)*CHUNK), end = '\r' )
+		data = stream.read(CHUNK)
+		frames.append(data)
+	print()
 	print("* done recording")
 
 	stream.stop_stream()
@@ -112,10 +168,48 @@ def record_mic_to_file(filename, choose_mic = False):
 	wf.setframerate(RATE)
 	wf.writeframes(b''.join(frames))
 	wf.close()
-	return filename
+
+	if input('Are you happy with that recording? [Y]/N: ').lower() in ['n', 'no']:
+		print(f'Rerecording...')
+		for j in range(3):
+			print(3-j, end = '\r')
+			time.sleep(1)
+		return record_mic_to_file(filename, choose_mic)
+	else:
+		return filename
 
 
+def wait(n):
+	for i in range(n):
+		time.sleep(1)
 
 
-if __name__ == '__main__':
+def main_old():
+	#Record Tracks from Microphone
+	print("Recording track 1")
+	t1_file = record_mic_to_file('track1.wav', True)
+	print("Recording track 2")
+	t2_file = record_mic_to_file('track2.wav', False)
+
+	#play two tracks back at the same time
+	t1 = AudioFile(t1_file)
+	t2 = AudioFile(t2_file)
+
+	t1_thread, t2_thread = play(t1), play(t2)
+
+	for i in range(3):
+		time.sleep(1)
+	#ERROR: These threads dont die!!!
+	print('done')
+
+def play_tracks_special():
+	print(f'Hello' )
+	track_files = ['special/track_0.wav', 'special/track_1.wav', 'special/track_2.wav']
+	for i in track_files:
+		play(AudioFile(i))
+	wait(RECORD_SECONDS)
+	print('Done')
+
+
+if __name__ == '__main__': #Call main() unless this file is being imported elsewhere
 	main()
